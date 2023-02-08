5f25d6d3b1a296d0dd86cfd46dcaf0b942b53be8
Matthias Kroiss <94349629+MatthiasKroiss@users.noreply.github.com>
2023-02-08 10:53:51 +0100

    Create typescript.js

---
+++ typescript.js
@@ -0,0 +1,25 @@
+
+var app = document.getElementById('heading');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('🍻')
+    .pauseFor(2500)
+    .deleteChars(2)
+    .typeString(':D')
+    .pauseFor(2500)
+    .deleteChars(2)
+    .typeString('ᓚᘏᗢ')
+    .pauseFor(2500)
+    .deleteChars(3)
+    .typeString('🥲')
+    .pauseFor(2500)
+    .deleteChars(2)
+    .typeString('🤓')
+    .pauseFor(2500)
+    .deleteChars(2)
+    .typeString('👍')
+    .pauseFor(2500)
+    .start();
