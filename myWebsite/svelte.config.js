d3b7c085b7c1b4df14e336709795af1421cd1749
MatthiasKroiss <94349629+MatthiasKroiss@users.noreply.github.com>
2023-02-27 14:43:35 +0100

    update

--- myWebsite/svelte.config.js
+++ myWebsite/svelte.config.js
@@ -1,14 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	//preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
+			pages: 'build',
+			assets : 'build',
 			fallback: '/'
 		}),
 		prerender: { entries: [] }
@@ -16,7 +20,7 @@ const config = {
 
 	trailingSlash: 'always',
         paths: {
-            base: '/MatthiasKroiss.github.io'
+			base: dev ? '' : '/matthiaskroiss.github.io',
         }
 };
 
--- myWebsite/vite.config.ts
+++ myWebsite/vite.config.ts
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: "/MatthiasKroiss.github.io",
+	base: "/MatthiasKroiss.github.io/",
 	plugins: [sveltekit()]
 });
