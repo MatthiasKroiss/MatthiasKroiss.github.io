2c4c5b7ce5ae4e776f967d337548ef509f5b5f27
Matthias Kroiss <94349629+MatthiasKroiss@users.noreply.github.com>
2023-02-27 12:24:31 +0100

    Delete _app directory

--- _app/immutable/chunks/2.fd104560.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.3e3562dd.js";export{t as component};
--- _app/immutable/chunks/3.9bae5f28.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/contactform-page.svelte.b62c8c20.js";export{t as component};
